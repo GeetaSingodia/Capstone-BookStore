04e6ac870e21d0f57a25a071ae762e601af8120f
geeta <g_singodia@yahoo.com>
2024-08-15 11:05:59 -0400

    commit my backend and frontend

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ import bookroute from "./route/book.route.js";
 import userRoute from './route/user.route.js';
 
 
+const path = require('path'); const { fileURLToPath } = require('url');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+const PORT = process.env.PORT || 4002;
+const URI = process.env.MONGODB_URI;
 
-// connect to mongoDB
+const __filename = fileURLToPath(import.meta.url); const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../../Frontend/dist')));
+
+// connect to MongoDB
 mongoose.connect(URI)
   .then(() => {
     console.log("Connected to MongoDB");
@@ -26,10 +31,18 @@ mongoose.connect(URI)
     console.error("Error connecting to MongoDB:", error);
   });
 
-// defining routes
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Bookstore API');
+});
+
+// Define routes
 app.use("/book", bookroute);
 app.use("/user", userRoute);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../Frontend/dist', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
